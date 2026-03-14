fae52dd8597c35cbe9099f2e66fe24002202cd4f
lakshitaparihar <omglakshita@gmail.com>
2026-03-14 12:16:14 +0530

    reorganised assignments, sem-wise

---
+++ sem1/func.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int addfunc(a,b){
+    c = a + b ;
+}
+int main(){
+    printf("inter the numbers to be added:");
+    int a, b;
+    scanf("%d %d", &a &b);
+    int add;
+    add= addfunc(a,b);
+    printf("sum is %d, add");
+}
