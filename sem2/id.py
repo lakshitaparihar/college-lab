39e8ed5afbf7d9812a72b7339085d273554b3d02
lakshitaparihar <omglakshita@gmail.com>
2026-04-27 08:26:59 +0530

    break overrr

---
+++ sem2/id.py
@@ -0,0 +1,24 @@
+l=[1,2,3,4,5,6,7,8,9,10]
+print(l)
+print("current address:",id(l))
+l.clear()
+print(l)
+print("address after:",id(l))
+print(len(l))
+
+
+# s="randomstring"
+# print(s)
+# print("current address:",id(s))
+# s+="extended"
+# print(s)
+# print("address after:",id(s))
+
+
+# l=[1,2,3,4,5,6,7,8,9,10]
+# print(l)
+# print("current address:",id(l))
+# l.append(11)
+# print(l)
+# print("address after:",id(l))
+
---
+++ sem2/listcomprehension.py
@@ -0,0 +1,3 @@
+#squaring numeric data stored in a list in a comprehensive way
+l=[1,2,3,4,5,6,7,8,9,10]
+for i in l:
